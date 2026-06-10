d28eb3934e165165051ecaa79dd2627ea79fd5b7
NickFeige <nfeige7@gmail.com>
2026-06-10 16:53:54 +0100

    Added background color

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,9 @@ import "./App.css";
 export default function HomePage() {
   return (
     <div className="container">
-      <h1>Nick Feige</h1> 
-      <button
-        onClick={() => alert("Hello!")}
-        className="button"
-      >
-        Click Me
-      </button>
+      <h1>Nick Feige</h1>
+      <h2>full stack developer</h2>
+     
     </div>
   );
 }
